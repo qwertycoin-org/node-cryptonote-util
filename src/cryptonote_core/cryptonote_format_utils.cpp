32bb1a9e5a7e01c1d3c723f274c2a8bafe0f7a4e
aiwe <maksymivv@gmail.com>
2018-05-13 15:23:52 +0300

    try minor again

--- src/cryptonote_core/cryptonote_format_utils.cpp
+++ src/cryptonote_core/cryptonote_format_utils.cpp
@@ -738,7 +738,7 @@ namespace cryptonote
     if(!get_block_hashing_blob(b, bd))
       return false;
 	cn_pow_hash_v2 ctx;
-	if (b.major_version >= BLOCK_MAJOR_VERSION_4)
+	if (b.minor_version >= BLOCK_MINOR_VERSION_1)
 	{
 		ctx.hash(bd.data(), bd.size(), res.data);
 	}
@@ -782,7 +782,7 @@ namespace cryptonote
     if(!get_bytecoin_block_hashing_blob(b, bd))
       return false;
 	cn_pow_hash_v2 ctx;
-	if (b.major_version >= BLOCK_MAJOR_VERSION_4)
+	if (b.minor_version >= BLOCK_MINOR_VERSION_1)
 	{
 		ctx.hash(bd.data(), bd.size(), res.data);
 	}
