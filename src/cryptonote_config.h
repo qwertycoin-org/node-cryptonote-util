ff50571ce6edbeae44d3e122d283bd853de12a3c
nnian <35841252+orangecoi@users.noreply.github.com>
2018-05-24 18:39:03 +0200

    Change bversion

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -2,15 +2,13 @@
 
 #define CRYPTONOTE_PUBLIC_ADDRESS_TEXTBLOB_VER          0
 #define CURRENT_TRANSACTION_VERSION                     1
-#define CURRENT_BLOCK_MAJOR_VERSION                     4
+#define CURRENT_BLOCK_MAJOR_VERSION                     3
 #define CURRENT_BLOCK_MINOR_VERSION                     0
 
 #define BLOCK_MAJOR_VERSION_1                           1
 #define BLOCK_MAJOR_VERSION_2                           2
 #define BLOCK_MAJOR_VERSION_3                           3
 #define BLOCK_MAJOR_VERSION_4                           4
-#define BLOCK_MAJOR_VERSION_5                           5
-#define BLOCK_MAJOR_VERSION_6                           6
 #define BLOCK_MINOR_VERSION_1                           1
 
 
